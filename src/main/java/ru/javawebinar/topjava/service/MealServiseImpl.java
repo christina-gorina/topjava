30067520e97703879dfcc326f89ee0146b784482
christina.gorina <christina.gorina@yandex.ru>
2020-06-11 18:33:14 +0300

    HW1. Optional. Fixes after code review 2

--- src/main/java/ru/javawebinar/topjava/service/MealServiseImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiseImpl.java
@@ -1,12 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.DAO.MealDao;
 import ru.javawebinar.topjava.DAO.MealDaoInMemory;
 import ru.javawebinar.topjava.model.Meal;
 
 import java.util.List;
 
 public class MealServiseImpl implements MealServise {
-    private MealDaoInMemory mealDAO = new MealDaoInMemory();
+    private MealDao mealDAO = new MealDaoInMemory();
 
     @Override
     public Meal add(Meal meal) {
