106bf1b4ede7d234f76bafdb081a4e86aa6eb6ec
christina.gorina <christina.gorina@yandex.ru>
2020-08-13 20:14:01 +0300

    Prepare to HW10. 10_04_opt_json_view.patch

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava;
 
-import javax.validation.groups.Default;
-
 public class View {
-    public interface Persist extends Default {}
+    public interface JsonREST {}
+    public interface JsonUI {}
 }
