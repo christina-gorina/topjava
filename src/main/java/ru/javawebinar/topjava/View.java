9372301c23a4044faa65a4eabb911d1323bd61e3
christina.gorina <christina.gorina@yandex.ru>
2020-08-13 20:14:37 +0300

    Prepare to HW10. 10_05_opt_validated_groups.patch

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava;
 
+import javax.validation.groups.Default;
+
 public class View {
-    public interface JsonREST {}
-    public interface JsonUI {}
+    public interface Persist extends Default {}
 }
